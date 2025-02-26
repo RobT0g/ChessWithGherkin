a3e9e93f194d4ff609390de6965a035b8a5f1ed2
Robson <robsonluan273@hotmail.com>
2025-02-25 23:19:25 -0300

    Debugging

--- ChessGame/board_manager.py
+++ ChessGame/board_manager.py
@@ -13,6 +13,11 @@ class BoardManager:
         self.display = pygame.display.set_mode((self.screen_size, self.screen_size))
         pygame.display.set_caption('Chess Game')
         self.generate_board()
+        self.player_turn = False
+
+        self.highlighted_piece = None
+        self.highlighted_moves = []
+        self.highlighted_attacks = []
 
     def generate_board(self):
         self.board_surface = pygame.Surface((self.tile_size*8+9*2, self.tile_size*8+9*2))
@@ -25,10 +30,10 @@ class BoardManager:
         for i in range(8):
             self.board[1][i] = Pawn(False)
             self.board[6][i] = Pawn(True)
-        self.board[0][0] = Rook(False, 8)
-        self.board[0][7] = Rook(False, 8)
-        self.board[7][0] = Rook(True, 8)
-        self.board[7][7] = Rook(True, 8)
+        self.board[0][0] = Rook(False)
+        self.board[0][7] = Rook(False)
+        self.board[7][0] = Rook(True)
+        self.board[7][7] = Rook(True)
         self.board[0][1] = Knight(False)
         self.board[0][6] = Knight(False)
         self.board[7][1] = Knight(True)
@@ -62,14 +67,42 @@ class BoardManager:
         row = (y - self.border_size) // (self.tile_size + 2)
         print(f"Clicked on row: {row}, col: {col}")
 
+        if row < 0 or row >= 8 or col < 0 or col >= 8:
+            return
+
+        if (row, col) in self.highlighted_moves or (row, col) in self.highlighted_attacks:
+            self.move_piece(self.highlighted_piece, row, col)
+            self.highlighted_piece = None
+            self.highlighted_moves = []
+            self.highlighted_attacks = []
+            self.player_turn = not self.player_turn
+            return
+
         if not self.board[row][col]:
             return
         
-        piece = self.board[row][col]
-        print(f"Moves: {moves}")
-        print(f"Attacks: {attacks}")
+        if self.board[row][col].player != self.player_turn:
+            return
+        
+        self.highlighted_piece = self.board[row][col]
+        self.highlighted_moves = []
+        self.highlighted_attacks = []
 
-        self.highlight_possible_moves(moves, attacks, piece.name == "Pawn")
+        moves = self.highlighted_piece.get_possible_moves()
+        attacks = self.highlighted_piece.get_possible_attacks()
+        print(f"Possible moves: {moves}")
+        print(f"Possible attacks: {attacks}")
+
+        if self.highlighted_piece.get_movement_type() == 'jump':
+            self.highlight_possible_jump_moves(self.highlighted_piece)
+
+        else:
+            self.highlight_possible_stream_moves(self.highlighted_piece)
+
+    def move_piece(self, piece:Piece, row:int, col:int):
+        self.board[piece.x][piece.y] = None
+        self.board[row][col] = piece
+        piece.set_piece_position(row, col)
 
     def highlight_possible_jump_moves(self, piece:Piece):
         moves = piece.get_possible_moves()
@@ -78,18 +111,21 @@ class BoardManager:
             if self.board[move[0]][move[1]]:
                 continue
             
-            pygame.draw.rect(self.display, (0, 255, 0), (move[1]*self.tile_size + self.border_size + (move[1]+1)*2, move[0]*self.tile_size + self.border_size + (move[0]+1)*2, self.tile_size, self.tile_size))
-
+            self.highlighted_moves.append(move)
+            
         for attack in attacks:
             if not self.board[attack[0]][attack[1]]:
                 continue
             
-            pygame.draw.rect(self.display, (255, 0, 0), (attack[1]*self.tile_size + self.border_size + (attack[1]+1)*2, attack[0]*self.tile_size + self.border_size + (attack[0]+1)*2, self.tile_size, self.tile_size))
+            if self.board[attack[0]][attack[1]].player == piece.player:
+                continue
+
+            self.highlighted_attacks.append(attack)
         
     def highlight_possible_stream_moves(self, piece:Piece):
         moves = piece.get_possible_moves()
         attacks = piece.get_possible_attacks()
-        
+
         for move in moves:
             x, y = piece.get_piece_position()
             x += move[0]
@@ -100,17 +136,23 @@ class BoardManager:
                     break
 
                 if self.board[x][y]:
-                    pygame.draw.rect(self.display, (255, 0, 0), (y*self.tile_size + self.border_size + (y+1)*2, x*self.tile_size + self.border_size + (x+1)*2, self.tile_size, self.tile_size))
+                    self.highlighted_attacks.append((x, y))
                     break
                 
-                pygame.draw.rect(self.display, (0, 255, 0), (y*self.tile_size + self.border_size + (y+1)*2, x*self.tile_size + self.border_size + (x+1)*2, self.tile_size, self.tile_size))
+                self.highlighted_moves.append((x, y))
                 x += move[0]
                 y += move[1]
-
     
     def display_board(self):
         self.display.blit(self.board_surface, (self.border_size, self.border_size))
         pygame.draw.rect(self.display, (139, 69, 19), (0, 0, self.screen_size, self.screen_size), self.border_size)
+
+        for move in self.highlighted_moves:
+            pygame.draw.rect(self.display, (0, 255, 0, 100), (move[1]*self.tile_size + self.border_size + (move[1]+1)*2, move[0]*self.tile_size + self.border_size + (move[0]+1)*2, self.tile_size, self.tile_size))
+
+        for attack in self.highlighted_attacks:
+            pygame.draw.rect(self.display, (255, 0, 0, 100), (attack[1]*self.tile_size + self.border_size + (attack[1]+1)*2, attack[0]*self.tile_size + self.border_size + (attack[0]+1)*2, self.tile_size, self.tile_size))
+
         self.draw_pieces()
         pygame.display.flip()
 
