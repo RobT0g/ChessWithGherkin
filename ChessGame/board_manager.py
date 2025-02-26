69ad1cc3695d4b92c712feb406378182166bc304
Robson <robsonluan273@hotmail.com>
2025-02-25 21:15:26 -0300

    Finished interface

--- ChessGame/board_manager.py
+++ ChessGame/board_manager.py
@@ -8,17 +8,18 @@ class BoardManager:
     def __init__(self):
         self.board = [[None for i in range(8)] for j in range(8)]
         self.tile_size = 75
-        self.screen_size = self.tile_size*8 + 60 + 9*2
+        self.border_size = 15
+        self.screen_size = self.tile_size*8 + self.border_size*2 + 9*2
         self.display = pygame.display.set_mode((self.screen_size, self.screen_size))
         pygame.display.set_caption('Chess Game')
         self.generate_board()
 
     def generate_board(self):
-        self.board_surface = pygame.Surface((self.tile_size*8, self.tile_size*8))
+        self.board_surface = pygame.Surface((self.tile_size*8+9*2, self.tile_size*8+9*2))
         for i in range(8):
             for j in range(8):
-                color = (255, 255, 255) if (i+j)%2 == 0 else (0, 0, 0)
-                pygame.draw.rect(self.board_surface, color, (i*self.tile_size, j*self.tile_size, self.tile_size, self.tile_size))
+                color = (222, 184, 135) if (i+j)%2 == 0 else (139, 69, 19)
+                pygame.draw.rect(self.board_surface, color, (i*self.tile_size + (i+1)*2, j*self.tile_size + (j+1)*2, self.tile_size, self.tile_size))
 
     def set_initial_arrengement(self):
         for i in range(8):
@@ -50,10 +51,11 @@ class BoardManager:
                 if piece:
                     piece.load_icon()
                     icon = piece.get_icon()
-                    self.display.blit(icon, (j*self.tile_size + 30, i*self.tile_size + 30))
+                    self.display.blit(icon, (j*self.tile_size + self.border_size + (j+1)*2, i*self.tile_size + self.border_size + (i+1)*2))
 
     def display_board(self):
-        self.display.blit(self.board_surface, (30, 30))
+        self.display.blit(self.board_surface, (self.border_size, self.border_size))
+        pygame.draw.rect(self.display, (139, 69, 19), (0, 0, self.screen_size, self.screen_size), self.border_size)
         self.draw_pieces()
         pygame.display.flip()
 
