30f294c7024e31ef238f054c7bc490aed9afefce
Robson Luan <robsonluan273@hotmail.com>
2025-03-01 15:38:41 -0300

    Added new feature for pawn

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,12 @@
 from behave import *
 import time
 
-step_latency = 2
+step_latency = 0.1
+def before_step(context, step):
+    '''This function is called before each step is executed'''
+    pass
+
 def after_step(context, step):
+    '''This function is called after each step is executed'''
     context.chess_board.display_board()
     time.sleep(step_latency)
