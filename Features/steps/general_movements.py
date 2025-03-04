c1729487a4e9a020ac270e69a61befcc14ff2df4
Robson <robsonluan273@hotmail.com>
2025-03-04 12:04:33 -0300

    Updated highlighted color function

--- Features/steps/general_movements.py
+++ Features/steps/general_movements.py
@@ -109,20 +109,25 @@ def step_i_see_square_highlighted_in_yellow(context, row, column, highlight_colo
 def step_i_see_squares_highlighted_in_color(context, squares, highlight_color):
     squares = get_positions_list_from_string(squares)
     highlight_color = highlight_color.replace("'", '')
-    for square in squares:
-        row, column = square
-        
-        if highlight_color == 'yellow':
-            assert check_square_highlighted_in_yellow(context, row, column), f'Square {row} {column} not highlighted in yellow'
+    
+    highlight_function = None    
+    match highlight_color:
+        case 'yellow':
+            highlight_function = check_square_highlighted_in_yellow
 
-        elif highlight_color == 'green':
-            assert check_square_highlighted_in_green(context, row, column), f'Square {row} {column} not highlighted in green'
+        case 'green':
+            highlight_function = check_square_highlighted_in_green
 
-        elif highlight_color == 'red':
-            assert check_square_highlighted_in_red(context, row, column), f'Square {row} {column} not highlighted in red'
+        case 'red':
+            highlight_function = check_square_highlighted_in_red
 
-        else:
+        case _:
             assert False, f'Invalid highlight color: {highlight_color}'
+
+    for l in range(context.chess_board.board_length):
+        for w in range(context.chess_board.board_width):
+            assert highlight_function(context, l, w) == ((l, w) in squares), ...
+            f'Square {l} {w} {"" if highlight_function(context, l, w) else "not"} highlighted in {highlight_color}'
 
 @given("the {piece_color} {piece_type} in position {row} {column} has already moved")
 def step_set_piece_as_already_moved(context, piece_color, piece_type, row, column):
